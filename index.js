ced6c7f783597f47e9c992cf11885b1bb346d3f3
Abdulla Al Fahad <fahad@evidentbd.com>
2024-10-24 22:55:49 +0600

    fix: async function used

--- index.js
+++ index.js
@@ -9,34 +9,46 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-app.post("/user", (req, res) => {
+app.post("/user", async (req, res) => {
     const user = new User(req.body)
     
-    user.save().then(() => {
-        res.send(user)
-    }).catch(e => {
-        res.send(e).status(400)
-    })  
+    try {
+        await user.save();
+        res.send(user).status(201)
+    } catch(e) {
+        res.send(e).status(500)
+    }   
 })
 
-app.get("/users", (req, res) => {
-    User.find({}).then((users) => {
-        res.send(users)
-    }).catch(e => res.send().status(500))
+app.get("/users", async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users).status(200)
+
+    } catch(e) {
+        res.send().status(500)
+    }
+
+   
 })
 
-app.get("/users/:id/", (req, res) => {
+app.get("/users/:id/", async (req, res) => {
     const _id = req.params.id;
 
-    User.findById(_id).then((user) => {
-    res.send(user)
-}).catch(e => {
-    console.log(e);
-    res.status(404).send("not found")
-})
+    try{
+        const user = await User.findById(_id)
+        if(!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user)
+
+    } catch(e) {
+        res.status(500).send()
+    }
 })
 
-app.post("/task", (req, res) => {
+app.post("/task", async (req, res) => {
     const task = new Task(req.body);
 
     task.save().then(() => {
@@ -44,6 +56,27 @@ app.post("/task", (req, res) => {
     }).catch((e) => {
         res.status(400).send(e)
     })
+})
+
+app.get("/tasks", (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => res.send().status(500))
+})
+
+app.get("/tasks/:id/", (req, res) => {
+    const _id = req.params.id;
+    
+
+    Task.findById(_id).then((task) => {
+        
+        if(!task) {    
+            return res.status(404).send()
+        }
+
+        res.send(task)
+
+    }).catch(e => res.status(500).send())
 })
 
 app.listen(port, () => {
